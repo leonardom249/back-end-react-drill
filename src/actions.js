0f87a58b5b3871b56f24d7f01e85e7dffabc7aa6
Glitch (nettle-linen) <none>
2018-04-17 16:13:25 +0000

    🏕🗻 Updated with Glitch

---
+++ src/actions.js
@@ -0,0 +1,30 @@
+// This function will make an AJAX request to the Star Wars API
+// It will randomly fail for 25% of requests, and has a 0.5s delay artifically
+// inserted so you can check your loading state
+import {search} from './star-wars';
+
+export const SEARCH_CHARACTERS_REQUEST = 'SEARCH_CHARACTERS_REQUEST';
+export const searchCharactersRequest = () => ({
+    type: SEARCH_CHARACTERS_REQUEST
+});
+
+export const SEARCH_CHARACTERS_SUCCESS = 'SEARCH_CHARACTERS_SUCCESS';
+export const searchCharactersSuccess = characters => ({
+    type: SEARCH_CHARACTERS_SUCCESS,
+    characters
+});
+
+export const SEARCH_CHARACTERS_ERROR = 'SEARCH_CHARACTERS_ERROR';
+export const searchCharactersError = error => ({
+    type: SEARCH_CHARACTERS_ERROR,
+    error
+});
+
+export const searchCharacters = name => dispatch => {
+    dispatch(searchCharactersRequest());
+  search(name)
+  .then(dataOnCharacter=> dispatch(searchCharactersSuccess(dataOnCharacter))
+       )
+  .catch(err=>{console.log(err); dispatch(searchCharactersError(err))});
+};
+
---
+++ src/components/character-search.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import Spinner from 'react-spinkit';
+import {searchCharacters} from '../actions';
+
+export class CharacterSearch extends React.Component {
+    renderResults() {
+        if (this.props.loading) {
+            return <Spinner spinnerName="circle" noFadeIn />;
+        }
+
+        if (this.props.error) {
+            return <strong>{this.props.error}</strong>;
+        }
+
+        const characters = this.props.characters.map((character, index) => (
+            <li key={index}>{character}</li>
+        ));
+
+        return <ul className="character-search-results">{characters}</ul>;
+    }
+  
+  // const newSearch=(e)=>{
+  //     e.preventDefault();
+  //         return this.props.dispatch(searchCharacters(this.input.value)
+  // }
+
+    render() {
+        return (
+            <div className="character-search">
+                
+                <form
+                  onSubmit={e=> { e.preventDefault();
+                    this.props.dispatch(searchCharacters(this.input.value))
+                    }
+                  }
+                >
+                    <input type="search" name='input' ref={input => (this.input = input)} />
+                    <button>Search</button>
+                </form>
+                <ul className="character-search-results">
+                    {this.renderResults()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    characters: state.characters,
+    loading: state.loading,
+    error: state.error
+});
+
+export default connect(mapStateToProps)(CharacterSearch);
